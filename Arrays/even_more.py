b2c5d078f29e0dd0c0542c41c0d8d3d5bc10479e
RiamTeodoro <165964443+RiamTeodoro@users.noreply.github.com>
2025-10-19 23:20:00 -0300

    Bug Fix - Add even_more

---
+++ Arrays/even_more.py
@@ -0,0 +1,12 @@
+def rearrange(arr):
+    for i in range(1, len(arr)):
+        if i % 2 == 0:
+            if arr[i] > arr[i-1]:
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+            else:
+                if arr[i] < arr[i-1]:
+                    arr[i-1], arr[i] = arr[i], arr[i-1]
+    print(arr)
+
+arr = [ 1, 3, 2, 2, 5 ]
+rearrange(arr)
