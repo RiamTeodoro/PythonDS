8fc6c0b796152620667e2ec3be6aab3c1cfa9a7d
RiamTeodoro <165964443+RiamTeodoro@users.noreply.github.com>
2025-11-10 18:17:35 -0300

    bugfix - Add HackerHank and Weird

--- HackerHank/Weird.py
+++ HackerHank/Weird.py
@@ -3,14 +3,16 @@ number = int(input("Escolha um numero: "))
 if number % 2 == 1:
     print("Weird")
 
-elif number % 2 == 0 and n >= 1 and n <= 15:
+elif number % 2 == 0 and number >= 1 and n <= 15:
     print("Not Weird")
     
-elif number % 2 == 0 and n <= 20:
+elif number % 2 == 0 and number <= 20:
     print("Weird")
 
-elif number % 2 == 0 and n >= 21:
+elif number % 2 == 0 and number >= 21:
     print("Not Weird")
 else:
     print("Number not found")
-        
+
+
+    
